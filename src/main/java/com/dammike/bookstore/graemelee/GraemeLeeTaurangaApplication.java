ce8f1fec7a609cddab1c5f8139e1873bef8f0a5a
Dom <dammikesaman@gmail.com>
2020-12-21 21:57:41 +1300

    base setup

---
+++ src/main/java/com/dammike/bookstore/graemelee/GraemeLeeTaurangaApplication.java
@@ -0,0 +1,13 @@
+package com.dammike.bookstore.graemelee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraemeLeeTaurangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraemeLeeTaurangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dammike/bookstore/graemelee/GraemeLeeTaurangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dammike.bookstore.graemelee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraemeLeeTaurangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
